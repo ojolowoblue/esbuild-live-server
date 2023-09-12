a182585a30d73a0e65646ccd3857e99d83dd8043
ojolowoblue <104099474+ojolowoblue@users.noreply.github.com>
2023-09-12 12:35:29 +0100

    Revert "Remove Redundancies"

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import esbuild from "esbuild";
-import server from "create-serve";
+import server, { log, update } from "create-serve";
+import chokidar from "chokidar";
 
 const isWatchMode =
   process.argv.includes("--watch") || process.argv.includes("-w");
 
 const serveWithEsbuild = async (
   options = {},
-  serverOptions = { root: ".", port: 4000 }
+  serverOptions = { root: ".", port: 4000 },
+  watchDir = "."
 ) => {
   const buildResult = await esbuild
     .context({
@@ -17,9 +19,25 @@ const serveWithEsbuild = async (
   if (isWatchMode) {
     server.start(serverOptions);
 
+    const watcher = chokidar.watch(watchDir, {
+      ignored: /(^|[\/\\])\../, // ignore dot files like e.g .env
+      persistent: true,
+    });
+
+    watcher.on("all", (ev, path) => {
+      update(`Updating changes -> ${path}`);
+      buildResult.rebuild().catch((err) => {
+        server.update();
+        log(err);
+      });
+    });
+
+    watcher.on("error", (error) => log(error));
+
     buildResult.watch().then(() => {
       buildResult.rebuild().catch((err) => {
         server.update();
+        log(err);
       });
     });
   } else {
